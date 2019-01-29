e390f276f19e8a89864c523ba48dfb489199410c
nowl.driesten <25991@ma-web.nl>
2019-01-29 09:57:54 +0100

    Initial commit

---
+++ src/Igor.java
@@ -0,0 +1,19 @@
+public class Igor {
+
+    public Igor() {
+
+    }
+
+    boolean IsOpSlot = true;
+
+    public void OpenDeur() {
+
+        if (IsOpSlot == true) {
+            System.out.println("de deur is op slot");
+        } else {
+            System.out.println("de deur kan open");
+        }
+
+    }
+
+}
---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main
+{
+
+    public static void main(String[] args)
+    {
+        Igor testigor = new Igor();
+        testigor.OpenDeur();
+    }
+
+}
